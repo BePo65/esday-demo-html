8e665ed8548b2bf17f33796715f55b5648d034a9
Bernhard Pottler <bpottler@nexgo.de>
2025-08-10 06:34:05 +0200

    feat: update to esdayQ0.5.0 and use plugin AdvanceFormat

--- src/script.js
+++ src/script.js
@@ -1,14 +1,17 @@
 import { esday } from 'esday';
+import advancedFormatPlugin from 'esday/plugins/advancedFormat';
+
+esday.extend(advancedFormatPlugin)
 
 window.addEventListener("load", (event) => {
   const now = esday();
-  document.getElementById("date-now").textContent = `${now.format()}`;
+  document.getElementById("date-now").textContent = `${now.format('YYYY-MM-DD HH:mm:ss')}`;
 });
 
 const btnSet = document.getElementById("set-output");
 btnSet.addEventListener('click', (event) => {
   const in5Days = esday().add(5, 'day');
-  document.getElementById("date-then").textContent = `${in5Days.format()}`;
+  document.getElementById("date-then").textContent = `${in5Days.format('YYYY-MM-DD HH:mm:ss')}`;
 });
 
 const btnReset = document.getElementById("reset-output");
